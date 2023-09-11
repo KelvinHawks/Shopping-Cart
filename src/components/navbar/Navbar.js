bd6c84abb1094e0451d52b93a0035ad47bda41b0
kelvin <kevohkyme35@gmail.com>
2023-09-11 23:32:53 +0300

    Ecommerce navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='nav__items'>
+            <p>Cart</p>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+        
+    </nav>
+  )
+}
+
+export default Navbar
